4336677612922651081042ed7fedfe4d09ad24b8
NielZAR <ariaans.niels@gmail.com>
2020-05-12 14:52:04 +0200

    update db create script

--- DatabaseCreateScript.sql
+++ DatabaseCreateScript.sql
@@ -5,68 +5,71 @@ DROP DATABASE JiraSyncMachine
 CREATE DATABASE JiraSyncMachine
 
 
-IF EXISTS (SELECT 1
-   FROM sys.sysreferences r JOIN sys.sysobjects o ON (o.id = r.constid AND o.type = 'F')
-   WHERE r.fkeyid = object_id('JIRA_USER') AND o.name = 'FK_JIRA_USE_REFERENCE_SYNC_MACHINE')
-ALTER TABLE JIRA_USER
-   DROP CONSTRAINT FK_JIRA_USE_REFERENCE_SYNC_MACHINE
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('JIRA_USER') and o.name = 'FK_JIRA_USE_REFERENCE_SYNC_MAC')
+alter table JIRA_USER
+   drop constraint FK_JIRA_USE_REFERENCE_SYNC_MAC
 go
 
-IF EXISTS (SELECT 1
-           FROM  sysobjects
-           WHERE id = object_id('JIRA_USER')
-           AND   type = 'U')
-   DROP TABLE JIRA_USER
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('JIRA_USER')
+            and   type = 'U')
+   drop table JIRA_USER
 go
 
-IF EXISTS (SELECT 1
-           FROM  sysobjects
-           WHERE id = object_id('SYNC_MACHINE_ACCOUNT')
-           AND   type = 'U')
-   DROP TABLE SYNC_MACHINE_ACCOUNT
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('SYNC_MACHINE_ACCOUNT')
+            and   type = 'U')
+   drop table SYNC_MACHINE_ACCOUNT
 go
 
-IF EXISTS (SELECT 1
-           FROM  sysobjects
-           WHERE id = object_id('WORKLOG')
-           AND   type = 'U')
-   DROP TABLE WORKLOG
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('WORKLOG')
+            and   type = 'U')
+   drop table WORKLOG
 go
 
 /*==============================================================*/
 /* Table: JIRA_USER                                             */
 /*==============================================================*/
-CREATE TABLE JIRA_USER (
-   ORIGIN_INSTANTCE_USER_KEY VARCHAR(13)     NOT NULL,
-   DESTINATION_INSTANCE_USER_KEY VARCHAR(13) NOT NULL,
-   USER_ID              INT                  NOT NULL,
-   AUTO_SYNC            BIT                  NOT NULL,
-   CONSTRAINT PK_JIRA_USER PRIMARY KEY NONCLUSTERED (ORIGIN_INSTANTCE_USER_KEY)
+create table JIRA_USER (
+   USER_ID              int                  not null,
+   ORIGIN_INSTANCE_USER_KEY varchar(13)          not null,
+   DESTINATION_INSTANCE_USER_KEY varchar(13)          not null,
+   AUTO_SYNC            bit                  not null,
+   constraint PK_JIRA_USER primary key nonclustered (USER_ID),
+   constraint AK_ORIGIN_USER_KEY unique (),
+   constraint AK_DESTINATION_USE unique ()
 )
 go
 
 /*==============================================================*/
 /* Table: SYNC_MACHINE_ACCOUNT                                  */
 /*==============================================================*/
-CREATE TABLE SYNC_MACHINE_ACCOUNT (
-   USER_ID              INT                  NOT NULL,
-   USERNAME             VARCHAR(MAX)         NOT NULL,
-   PASSWORD             CHARACTER(30)        NOT NULL,
-   CONSTRAINT PK_SYNC_MACHINE_ACCOUNT PRIMARY KEY (USER_ID)
+create table SYNC_MACHINE_ACCOUNT (
+   USER_ID              int                  not null,
+   USERNAME             varchar(Max)         not null,
+   PASSWORD             character(30)        not null,
+   constraint PK_SYNC_MACHINE_ACCOUNT primary key (USER_ID)
 )
 go
 
 /*==============================================================*/
 /* Table: WORKLOG                                               */
 /*==============================================================*/
-CREATE TABLE WORKLOG (
-   WORKLOG_ID           INT                  NOT NULL,
-   CONSTRAINT PK_WORKLOG PRIMARY KEY (WORKLOG_ID)
+create table WORKLOG (
+   WORKLOG_ID           int                  not null,
+   constraint PK_WORKLOG primary key (WORKLOG_ID)
 )
 go
 
-ALTER TABLE JIRA_USER
-   ADD CONSTRAINT FK_JIRA_USE_REFERENCE_SYNC_MACHINE FOREIGN KEY (USER_ID)
-      REFERENCES SYNC_MACHINE_ACCOUNT (USER_ID)
-         ON UPDATE CASCADE ON DELETE CASCADE
+alter table JIRA_USER
+   add constraint FK_JIRA_USE_REFERENCE_SYNC_MAC foreign key (USER_ID)
+      references SYNC_MACHINE_ACCOUNT (USER_ID)
+         on update cascade on delete cascade
 go
+
