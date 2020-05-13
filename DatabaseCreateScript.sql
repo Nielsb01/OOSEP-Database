db01543c19c27abef749398c5202b3b84c44f2f0
Niels Ariaans <NAJ.Ariaans@student.han.nl>
2020-05-13 11:11:11 +0200

    DB create script update
    Text formatting verbeterd.

--- DatabaseCreateScript.sql
+++ DatabaseCreateScript.sql
@@ -1,75 +1,50 @@
+/* Database Opzetten */
+
 USE master
 IF EXISTS(SELECT * FROM sys.databases WHERE NAME='JiraSyncMachine')
 DROP DATABASE JiraSyncMachine
 
 CREATE DATABASE JiraSyncMachine
+USE JiraSyncMachine
 
-
-if exists (select 1
-   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('JIRA_USER') and o.name = 'FK_JIRA_USE_REFERENCE_SYNC_MAC')
-alter table JIRA_USER
-   drop constraint FK_JIRA_USE_REFERENCE_SYNC_MAC
-go
-
-if exists (select 1
-            from  sysobjects
-           where  id = object_id('JIRA_USER')
-            and   type = 'U')
-   drop table JIRA_USER
-go
-
-if exists (select 1
-            from  sysobjects
-           where  id = object_id('SYNC_MACHINE_ACCOUNT')
-            and   type = 'U')
-   drop table SYNC_MACHINE_ACCOUNT
-go
-
-if exists (select 1
-            from  sysobjects
-           where  id = object_id('WORKLOG')
-            and   type = 'U')
-   drop table WORKLOG
-go
 
 /*==============================================================*/
-/* Table: JIRA_USER                                             */
+/* TABLE: jira_user                                             */
 /*==============================================================*/
-create table JIRA_USER (
-   USER_ID              int                  not null,
-   ORIGIN_INSTANCE_USER_KEY varchar(13)          not null,
-   DESTINATION_INSTANCE_USER_KEY varchar(13)          not null,
-   AUTO_SYNC            bit                  not null,
-   constraint PK_JIRA_USER primary key nonclustered (USER_ID),
-   constraint AK_ORIGIN_USER_KEY unique (),
-   constraint AK_DESTINATION_USE unique ()
+CREATE TABLE jira_user (
+   user_id							INT                  NOT NULL,
+   origin_instance_user_key		VARCHAR(13)          NOT NULL,
+   destination_instance_user_key	VARCHAR(13)          NOT NULL,
+   auto_sync						BIT                  NOT NULL,
+   CONSTRAINT PK_JIRA_USER PRIMARY KEY NONCLUSTERED (user_id),
+   CONSTRAINT AK_ORIGIN_USER_KEY UNIQUE (origin_instance_user_key),
+   CONSTRAINT AK_DESTINATION_USER_KEY UNIQUE (destination_instance_user_key)
 )
-go
+GO
 
 /*==============================================================*/
-/* Table: SYNC_MACHINE_ACCOUNT                                  */
+/* TABLE: sync_machine_account                                  */
 /*==============================================================*/
-create table SYNC_MACHINE_ACCOUNT (
-   USER_ID              int                  not null,
-   USERNAME             varchar(Max)         not null,
-   PASSWORD             character(30)        not null,
-   constraint PK_SYNC_MACHINE_ACCOUNT primary key (USER_ID)
+CREATE TABLE sync_machine_account (
+   user_id              INT                  NOT NULL,
+   username             VARCHAR(Max)         NOT NULL,
+   password             CHARACTER(30)        NOT NULL,
+   CONSTRAINT PK_SYNC_MACHINE_ACCOUNT PRIMARY KEY (user_id)
 )
-go
+GO
 
 /*==============================================================*/
-/* Table: WORKLOG                                               */
+/* TABLE: worklog                                               */
 /*==============================================================*/
-create table WORKLOG (
-   WORKLOG_ID           int                  not null,
-   constraint PK_WORKLOG primary key (WORKLOG_ID)
+CREATE TABLE worklog (
+   worklog_id			 INT                  NOT NULL,
+   CONSTRAINT PK_WORKLOG PRIMARY KEY (worklog_id)
 )
-go
+GO
 
-alter table JIRA_USER
-   add constraint FK_JIRA_USE_REFERENCE_SYNC_MAC foreign key (USER_ID)
-      references SYNC_MACHINE_ACCOUNT (USER_ID)
-         on update cascade on delete cascade
-go
 
+ALTER TABLE jira_user
+   ADD CONSTRAINT FK_SYNC_MACHINE_ACCOUNT_JIRA_USER FOREIGN KEY (user_id)
+      REFERENCES sync_machine_account (user_id)
+         ON UPDATE CASCADE ON DELETE CASCADE
+GO
