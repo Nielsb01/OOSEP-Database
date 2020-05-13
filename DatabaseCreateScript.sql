766c2477e225f55689dd48d78b49605969d3d62c
max1e <47426263+max1e@users.noreply.github.com>
2020-05-13 14:44:25 +0200

    createscript geupdatet en insertscript toegevoegd

--- DatabaseCreateScript.sql
+++ DatabaseCreateScript.sql
@@ -1,37 +1,37 @@
-/* Database Opzetten */
+/* Database opzetten */
 
-USE master
-IF EXISTS(SELECT * FROM sys.databases WHERE NAME='JiraSyncMachine')
-DROP DATABASE JiraSyncMachine
+USE sys;
+DROP SCHEMA IF EXISTS JiraSyncMachine;
 
-CREATE DATABASE JiraSyncMachine
-USE JiraSyncMachine
+CREATE DATABASE JiraSyncMachine;
+USE JiraSyncMachine;
 
+/*==============================================================*/
+/* TABLE: sync_machine_account                                  */
+/*==============================================================*/
+CREATE TABLE sync_machine_account (
+   user_id              INT AUTO_INCREMENT  NOT NULL,
+   username             VARCHAR(30)         NOT NULL,
+   password             CHARACTER(30)       NOT NULL,
+   CONSTRAINT PK_SYNC_MACHINE_ACCOUNT PRIMARY KEY (user_id)
+);
 
 /*==============================================================*/
 /* TABLE: jira_user                                             */
 /*==============================================================*/
 CREATE TABLE jira_user (
    user_id							INT                  NOT NULL,
-   origin_instance_user_key			VARCHAR(13)          NOT NULL,
-   destination_instance_user_key	VARCHAR(13)          NOT NULL,
+   origin_instance_user_key			CHAR(13)          NOT NULL,
+   destination_instance_user_key	CHAR(13)          NOT NULL,
    auto_sync						BIT                  NOT NULL,
    CONSTRAINT PK_JIRA_USER PRIMARY KEY NONCLUSTERED (user_id),
    CONSTRAINT AK_ORIGIN_USER_KEY UNIQUE (origin_instance_user_key),
-   CONSTRAINT AK_DESTINATION_USER_KEY UNIQUE (destination_instance_user_key)
-)
-GO
-
-/*==============================================================*/
-/* TABLE: sync_machine_account                                  */
-/*==============================================================*/
-CREATE TABLE sync_machine_account (
-   user_id              INT                  NOT NULL,
-   username             VARCHAR(Max)         NOT NULL,
-   password             CHARACTER(30)        NOT NULL,
-   CONSTRAINT PK_SYNC_MACHINE_ACCOUNT PRIMARY KEY (user_id)
-)
-GO
+   CONSTRAINT AK_DESTINATION_USER_KEY UNIQUE (destination_instance_user_key),
+   CONSTRAINT FK_SYNC_MACHINE_ACCOUNT_JIRA_USER FOREIGN KEY (user_id)
+      REFERENCES sync_machine_account (user_id)
+         ON UPDATE CASCADE 
+         ON DELETE CASCADE
+);
 
 /*==============================================================*/
 /* TABLE: worklog                                               */
@@ -39,12 +39,4 @@ GO
 CREATE TABLE worklog (
    worklog_id			 INT                  NOT NULL,
    CONSTRAINT PK_WORKLOG PRIMARY KEY (worklog_id)
-)
-GO
-
-
-ALTER TABLE jira_user
-   ADD CONSTRAINT FK_SYNC_MACHINE_ACCOUNT_JIRA_USER FOREIGN KEY (user_id)
-      REFERENCES sync_machine_account (user_id)
-         ON UPDATE CASCADE ON DELETE CASCADE
-GO
+);
